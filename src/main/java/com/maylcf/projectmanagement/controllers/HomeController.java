f93217347ea0eb8b7f66f7075fc5767117492979
Mayara Felix <mayaralcabral@gmail.com>
2021-02-02 23:24:56 -0500

    Move home layout to inside "main" folder.

--- src/main/java/com/maylcf/projectmanagement/controllers/HomeController.java
+++ src/main/java/com/maylcf/projectmanagement/controllers/HomeController.java
@@ -28,6 +28,6 @@ public class HomeController {
         model.addAttribute("projects", projects);
         model.addAttribute("employees", employees);
 
-        return "home";
+        return "main/home";
     }
 }
