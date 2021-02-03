14ac07de6d7acd2767ae8775871bb5a95b928d1b
Mayara Felix <mayaralcabral@gmail.com>
2021-02-03 18:03:27 -0500

    Revert "Lecture 18 - Update Navigation"

--- src/main/java/com/maylcf/projectmanagement/controllers/ProjectController.java
+++ src/main/java/com/maylcf/projectmanagement/controllers/ProjectController.java
@@ -1,41 +1,25 @@
 package com.maylcf.projectmanagement.controllers;
 
-import com.maylcf.projectmanagement.dao.ProjectRepository;
 import com.maylcf.projectmanagement.entities.Project;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
 
-    @Autowired
-    ProjectRepository repository;
-
-    @GetMapping
-    public String displayProjects(Model model) {
-        List<Project> projects = repository.findAll();
-        model.addAttribute("projects", projects);
-        return "projects/list-projects";
-    }
-
     @GetMapping("/new")
     public String displayProjectForm(Model model) {
         model.addAttribute("project", new Project());
-        return "projects/new-project";
+        return "new-project";
     }
 
     @PostMapping("/save")
     public String createProject(Project project, Model model) {
-        repository.save(project);
-
-        //use a redirect to prevent duplicate submissions.
-        return "redirect:/projects/new";
+        // this should handle savings the project into the database.
+        return "";
     }
 }
