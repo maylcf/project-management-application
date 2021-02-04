2eda46fc6028578abab3813bf6ecb511a2ad6daf
Mayara Felix <mayaralcabral@gmail.com>
2021-02-03 23:56:21 -0500

    - Update project controller to send a list of employees in the "/new" mapping. - Update "/save" mapping to save the selected employees.

--- src/main/java/com/maylcf/projectmanagement/controllers/ProjectController.java
+++ src/main/java/com/maylcf/projectmanagement/controllers/ProjectController.java
@@ -1,6 +1,8 @@
 package com.maylcf.projectmanagement.controllers;
 
+import com.maylcf.projectmanagement.dao.EmployeeRepository;
 import com.maylcf.projectmanagement.dao.ProjectRepository;
+import com.maylcf.projectmanagement.entities.Employee;
 import com.maylcf.projectmanagement.entities.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,6 +20,9 @@ public class ProjectController {
 
     @Autowired
     ProjectRepository repository;
+
+    @Autowired
+    EmployeeRepository employeeRepo;
 
     @GetMapping
     public String displayProjects(Model model) {
@@ -28,12 +34,20 @@ public class ProjectController {
     @GetMapping("/new")
     public String displayProjectForm(Model model) {
         model.addAttribute("project", new Project());
+        model.addAttribute("employees", employeeRepo.findAll());
+
         return "projects/new-project";
     }
 
     @PostMapping("/save")
-    public String createProject(Project project, Model model) {
+    public String createProject(Project project, @RequestParam List<Long> employees, Model model) {
         repository.save(project);
+
+        Iterable<Employee> selectedEmployees = employeeRepo.findAllById(employees);
+        for (Employee employee : selectedEmployees) {
+            employee.setProject(project);
+            employeeRepo.save(employee);
+        }
 
         //use a redirect to prevent duplicate submissions.
         return "redirect:/projects/new";
