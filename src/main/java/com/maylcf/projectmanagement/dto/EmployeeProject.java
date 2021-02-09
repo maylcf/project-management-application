20671b6e128ebdda1e25786a90e21f82f871a00b
Mayara Felix <mayaralcabral@gmail.com>
2021-02-08 23:20:39 -0500

    Create an interface for the employee x project query.

---
+++ src/main/java/com/maylcf/projectmanagement/dto/EmployeeProject.java
@@ -0,0 +1,10 @@
+package com.maylcf.projectmanagement.dto;
+
+/**
+ * We need to add the property name stating with get.
+ */
+public interface EmployeeProject {
+    public String getFirstName();
+    public String getLastName();
+    public int getProjectCount();
+}
