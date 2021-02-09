fd67b81f8c0d391ff61cfd108e75368246c930da
Mayara Felix <mayaralcabral@gmail.com>
2021-02-08 23:20:11 -0500

    Create new function to return the number of projects that a employee is associated with in EmployeeRepository.

--- src/main/java/com/maylcf/projectmanagement/dao/EmployeeRepository.java
+++ src/main/java/com/maylcf/projectmanagement/dao/EmployeeRepository.java
@@ -1,6 +1,9 @@
 package com.maylcf.projectmanagement.dao;
 
+import com.maylcf.projectmanagement.dto.EmployeeProject;
 import com.maylcf.projectmanagement.entities.Employee;
+import com.maylcf.projectmanagement.util.Queries;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +11,7 @@ import java.util.List;
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
     @Override
     public List<Employee> findAll();
+
+    @Query(nativeQuery = true, value = Queries.EMPLOYEE_PROJECT)
+    public List<EmployeeProject> employeeProject();
 }
