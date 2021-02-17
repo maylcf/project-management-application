cc684a01d6f86495dc80f9102dc68df7b8271d5b
maylcf <mayaralcabral@gmail.com>
2021-02-16 22:47:18 -0500

    Remove unnecessary annotations.

--- src/test/java/com/maylcf/projectmanagement/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/maylcf/projectmanagement/ProjectRepositoryIntegrationTest.java
@@ -5,17 +5,15 @@ import com.maylcf.projectmanagement.entities.Project;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 @SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"})})
 public class ProjectRepositoryIntegrationTest {
 
