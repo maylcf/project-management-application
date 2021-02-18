5d84699452ce8ce5e652ecd10ef4ef50150cb44b
Mayara Felix <mayaralcabral@gmail.com>
2021-02-17 21:55:56 -0500

    Create WebConfig class to generate a passwordEncoder.

---
+++ src/main/java/com/maylcf/projectmanagement/security/WebConfig.java
@@ -0,0 +1,13 @@
+package com.maylcf.projectmanagement.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
